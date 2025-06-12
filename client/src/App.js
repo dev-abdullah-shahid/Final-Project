f22a4effd6a1e312fb2460378ef2b177e2c5066e
Abdullah <urfvabdullah@gmail.com>
2025-06-12 19:40:03 +0500

    Front end vite issue resolving

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,17 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Home from './pages/Home';
-import Projects from './pages/Projects';
-import About from './pages/About';
+import './App.css';
 
 function App() {
-    return (
-        <div>
-            <Header />
-            <Switch>
-                <Route path="/" exact component={Home} />
-                <Route path="/projects" component={Projects} />
-                <Route path="/about" component={About} />
-            </Switch>
-            <Footer />
-        </div>
-    );
+  return (
+    <div>
+      <Header />
+      <Home />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
