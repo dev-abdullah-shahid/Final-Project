2e9c688000f824201fdc085963ff47d8ff9d36ce
Abdullah <urfvabdullah@gmail.com>
2025-06-12 20:52:12 +0500

    Gallery addition

--- client/src/pages/Gallery.jsx
+++ client/src/pages/Gallery.jsx
@@ -1,13 +1,26 @@
-// client/src/pages/Gallery.jsx
-
 import React from 'react';
+import './Gallery.css'; // Make sure to create this CSS file
 
 const Gallery = () => {
+  const images = [
+    // Add your image URLs here
+    'image1.jpg',
+    'image2.jpg',
+    'image3.jpg',
+    // Continue adding images...
+  ];
+
   return (
     <div>
       <h2>Gallery</h2>
       <p>Welcome to the DIY Gallery!</p>
-      {/* Add gallery content here */}
+      <div className="gallery">
+        {images.map((image, index) => (
+          <div className="gallery-item" key={index}>
+            <img src={image} alt={`Gallery item ${index + 1}`} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
